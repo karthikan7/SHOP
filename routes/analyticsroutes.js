4aea5084ec77bb509e17e4bc81e54dac36b87e67
Karthik A N <ankarthik657@gmail.com>
2026-06-26 17:37:12 +0530

    remove backend files

--- routes/analyticsroutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getAdminStats } = require('../controllers/analyticcontroller');
-const { protect, admin } = require('../middleware/authmiddaleware');
-
-const router = express.Router();
-
-router.get('/', protect, admin, getAdminStats);
-
-module.exports = router;
